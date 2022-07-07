7a58bf5ff565bf4a1bd3f00c2e6efcb9d5d37b2a
baaqerfarhat <53981711+baaqerfarhat@users.noreply.github.com>
2022-07-07 11:41:42 -0500

    Change To TalonSRX

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,13 +8,17 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.Talon;
+
+
+
 
 /** This is a demo program showing how to use Mecanum control with the MecanumDrive class. */
 public class Robot extends TimedRobot {
-  private static final int kFrontLeftChannel = 2;
-  private static final int kRearLeftChannel = 3;
-  private static final int kFrontRightChannel = 1;
-  private static final int kRearRightChannel = 0;
+  private static final int kFrontLeftChannel = 3;
+  private static final int kRearLeftChannel = 2;
+  private static final int kFrontRightChannel = 0;
+  private static final int kRearRightChannel = 1;
 
   private static final int kJoystickChannel = 0;
 
@@ -23,10 +27,10 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
-    PWMSparkMax frontLeft = new PWMSparkMax(kFrontLeftChannel);
-    PWMSparkMax rearLeft = new PWMSparkMax(kRearLeftChannel);
-    PWMSparkMax frontRight = new PWMSparkMax(kFrontRightChannel);
-    PWMSparkMax rearRight = new PWMSparkMax(kRearRightChannel);
+    TalonSRX frontLeft = new TalonSRX(kFrontLeftChannel);
+    TalonSRX rearLeft = new TalonSRX(kRearLeftChannel);
+    TalonSRX frontRight = new TalonSRX(kFrontRightChannel);
+    TalonSRX rearRight = new TalonSRX(kRearRightChannel);
 
     // Invert the right side motors.
     // You may need to change or remove this to match your robot.
